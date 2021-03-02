eba0158885c119e7132d1695eb86aea2b3af0b36
Yuki Yoshida <abs@sfc.keio.ac.jp>
2021-03-02 11:17:15 +0900

    Create SiteDefs.pm

---
+++ conf/SiteDefs.pm
@@ -0,0 +1,39 @@
+package EG::YourPlugin::SiteDefs;
+
+use strict;
+
+sub update_conf {
+    push @$SiteDefs::ENSEMBL_API_LIBS, $SiteDefs::ENSEMBL_SERVERROOT.'/PolypedilumHub/modules';
+
+    $SiteDefs::SITE_LOGO_ALT = 'MidgeBase Ensembl';
+    $SiteDefs::SITE_NAME = 'MidgeBase Ensembl';
+    
+    $SiteDefs::ISSUE_TRACKER_URL = 'https://github.com/genomehubs/genomehubs/issues?status=new&status=open';
+    $SiteDefs::ISSUE_TRACKER_TITLE = 'report an issue';
+    
+    
+    $SiteDefs::ASSEMBLY_GROUP_A = [
+            'Polypedilum_vanderplanki_Pv52',
+            'Polypedilum_pembai_pp1',
+            'Polypedilum_nubifer_pn09'
+    ];
+    $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Polypedilum';
+    $SiteDefs::ASSEMBLY_GROUP_A_TEMPLATE = '_fav_template';
+    $SiteDefs::ASSEMBLY_GROUP_B = [
+            'Chironomus_riparius_ASM101450',
+            'Chironomus_tentans_ct01'
+    ];
+    $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Chironomus';
+    $SiteDefs::ASSEMBLY_GROUP_B_TEMPLATE = '_fav_template';
+    $SiteDefs::ASSEMBLY_GROUP_C = [
+            'Belgica_antarctica',
+            'Aedes_aegypti',
+            'Anopheles_gambiae',
+            'Culex_quinquefasciatus', 
+            'Drosophila_melanogaster'
+    ];
+    $SiteDefs::ASSEMBLY_GROUP_C_TITLE = 'Other Diptera';
+    $SiteDefs::ASSEMBLY_GROUP_C_TEMPLATE = '_fav_template';
+  
+}
+1;
